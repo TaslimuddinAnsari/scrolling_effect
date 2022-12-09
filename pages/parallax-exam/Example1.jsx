3add831f8c9e1e9f3b24c46c2f3ba8c1447255cd
TaslimuddinAnsari <taslimuddin.ansari@madgicaltechdom.com>
2022-12-09 17:22:40 +0530

    Parallax demo-2

---
+++ pages/parallax-exam/Example1.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Example1 = () => {
+  return (
+    <div className='wrapper'>
+      <header>
+        <img src='https://tse2.mm.bing.net/th?id=OIP.PR3Pgqc5fS2Yk3hbbO3erQHaDy&pid=Api&P=0' 
+        className='background' />
+        {/* <img src='https://tse4.mm.bing.net/th?id=OIP.ntPkhMKhtgr4ZYgrAUycMAHaE7&pid=Api&P=0' 
+        className='foreground' /> */}
+        <h1 className='heading_text'>Welcome!</h1>
+      </header>
+      <section>
+        <div className='description'>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Eos omnis iste deserunt 
+        veritatis expedita dolor asperiores necessitatibus 
+        nobis incidunt magni dolorem, repudiandae ab. Explicabo
+         ex sunt eum error! Consequatur quibusdam rem, architecto 
+         tempore sed aspernatur esse est doloremque dolorum minima 
+         repellat ipsum enim, illum natus ratione at, commodi eveniet 
+         ipsam. Excepturi esse magnam consequuntur aliquam ex dolorem 
+         culpa ut ratione tenetur, architecto provident dolores. Sit 
+         officiis tempore dicta maiores velit laboriosam, vero earum 
+         placeat et vel ullam itaque, reiciendis ea nam quaerat ex? Iusto 
+         necessitatibus, accusamus odio praesentium ipsam quibusdam sit 
+         minus maiores vero placeat aliquid eaque, culpa cupiditate iste 
+         quia sunt eius totam illo animi? Quod nesciunt quasi omnis quam 
+         qui et illum esse labore laudantium adipisci, laborum suscipit dicta. 
+         Vero sunt earum, facere voluptatibus delectus quos, dolorum maxime in
+         corporis distinctio expedita? Libero ducimus quaerat aut mollitia ipsum? 
+         Reiciendis id enim accusamus, rem distinctio earum culpa eveniet similique 
+         adipisci in expedita dicta consequuntur temporibus libero natus perferendis 
+         ut omnis soluta ab nihil commodi ad assumenda aliquid ipsam. Blanditiis, 
+         dolorum est. Sed voluptatum, sunt sint repudiandae nemo qui nostrum laborum
+          ea aliquid repellat dolores laudantium nisi accusantium accusamus atque
+           molestiae deserunt ab cupiditate beatae. Numquam, consequatur esse. Qui 
+           cumque ea porro perspiciatis doloribus ratione dolore, suscipit accusantium 
+           rerum eaque quo, nostrum voluptates obcaecati, aliquam ut repellendus 
+           explicabo sint. Libero possimus consequuntur sapiente saepe! Totam dolores 
+           mollitia, quasi repellendus esse ducimus molestias tenetur nihil eius 
+           architecto illo laudantium repellat eveniet repudiandae iure magni dolore
+            ratione in. Repudiandae aliquid odio non? Ut optio repellat modi nostrum 
+            voluptas fugiat doloribus doloremque? Vitae similique natus voluptatem non?
+             Ducimus laboriosam esse nihil eligendi a, maiores nostrum consectetur 
+             atque odio reprehenderit tempore mollitia numquam ullam eos vel ipsum 
+             assumenda rem officia inventore error repellat doloremque voluptates? 
+        Atque inventore placeat et nostrum nam dolorum, suscipit quod!
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Example1
